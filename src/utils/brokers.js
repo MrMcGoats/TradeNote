f02af14bbbe1c16c26d474d5b12ffca0e618aa30
Jacob <31115470+MrMcGoats@users.noreply.github.com>
2025-06-06 15:30:10 -0700

    Fixed various bugs

--- src/utils/brokers.js
+++ src/utils/brokers.js
@@ -1037,6 +1037,10 @@ export async function useNinjaTrader(param) {
             //we need to recreate the JSON with proper date format + we simplify
             //console.log("papaparse " + JSON.stringify(papaParse.data))
             
+            // Sort data by date (formated as YYYY-MM-DD HH:mm:ss)
+            papaParse.data.sort((a, b) => dayjs(a["Time"], "YYYY-MM-DD HH:mm:ss") - dayjs(b["Time"], "YYYY-MM-DD HH:mm:ss"));
+
+            
             // Track the current position for each account/symbol combination
             // We will use this for the "Entry/Exit" orders
             let currentPosition = {}
